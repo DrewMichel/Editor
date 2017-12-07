b18a35e263c46609fca526e3078b44c5ef58e2d4
Andrew Michel <drewmichel33@gmail.com>
2017-12-06 16:23:40 -0800

    Updating.  EditorKeyPressedListener is now an inner class of Main.  Added EditorKeyReleasedListener as another inner class to Main as well.

--- src/primary/Main.java
+++ src/primary/Main.java
@@ -2,21 +2,33 @@ package primary;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.io.File;
+import java.util.HashMap;
+
 /**
  * Created by Andrew Michel on 12/6/2017.
  */
+// TODO: Should keyMappings be used if the inherit key delay would be desired?
 public class Main extends Application
 {
     public static final int DEFAULT_WINDOW_WIDTH = 1080, DEFAULT_WINDOW_HEIGHT = 720;
 
+    private HashMap<KeyCode, Boolean> keyMappings;
+
     private Stage primaryStage;
     private Pane primaryPane;
     private Scene primaryScene;
+
+    // Enforce generics on declaration and initialization?
+    private EventHandler keyPressedListener, keyReleasedListener;
 
     @Override
     public void start(Stage primaryStage) throws Exception
@@ -36,6 +48,54 @@ public class Main extends Application
 
         primaryStage.isFocused();
 
+        keyPressedListener = new EditorKeyPressedListener();
+        keyReleasedListener = new EditorKeyReleasedListener();
+
+        // Add to primaryPane or deeper pane?
+        primaryPane.setOnKeyPressed(keyPressedListener);
+        primaryPane.setOnKeyReleased(keyReleasedListener);
+        primaryPane.requestFocus();
+
         primaryStage.show();
     }
+
+    private boolean populateKeyMappings(File path)
+    {
+        return true;
+    }
+
+    private boolean populateKeyMappings(String path)
+    {
+        return populateKeyMappings(new File(path));
+    }
+
+    private class EditorKeyPressedListener<T extends KeyEvent> implements EventHandler<T>
+    {
+        public EditorKeyPressedListener()
+        {
+            super();
+        }
+
+        @Override
+        public void handle(T event)
+        {
+            System.err.println(event);
+        }
+    }
+
+    private class EditorKeyReleasedListener<T extends KeyEvent> implements EventHandler<T>
+    {
+        public EditorKeyReleasedListener()
+        {
+            super();
+        }
+
+        @Override
+        public void handle(T event)
+        {
+            System.err.println(event);
+        }
+    }
+
 }
+
